aadbc3259be80f64ab1a12660633e0e1d5edbed4
RomanLomakin <lomakinrs1997@yandex.ru>
2022-05-07 12:41:48 +0300

    commit

---
+++ app/src/main/java/com/bignerdranch/android/marvelpetproject/domain/entity/MarvelData.kt
@@ -0,0 +1,23 @@
+package com.bignerdranch.android.marvelpetproject.domain.entity
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class MarvelData(
+    @SerializedName("bio")
+    val bio: String,
+    @SerializedName("createdby")
+    val createdBy: String,
+    @SerializedName("firstappearance")
+    val firstAppearance: String,
+    @SerializedName("imageurl")
+    val imageUrl: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("publisher")
+    val publisher: String,
+    @SerializedName("realname")
+    val realName: String,
+    @SerializedName("team")
+    val team: String,
+) : Serializable
